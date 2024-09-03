df2645e98d46897075e9fe1b1f53d17793e3bae8
이승지 <162680066+LeeSeungJi27@users.noreply.github.com>
2024-09-03 20:57:55 +0900

    header,footer front수정

--- node_modules/test/src/component/Footer.jsx
+++ node_modules/test/src/component/Footer.jsx
@@ -8,39 +8,41 @@ const Footer = () => {
         <nav className="pull-left">
           <ul className="nav">
             <li className="nav-item">
-              <a className="nav-link" href="http://www.themekita.com">
-                ThemeKita
+              <a className="nav-link" href="https://github.com/hyoj1201">
+                곽효정
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                {' '}
-                Help{' '}
+              <a className="nav-link" href="https://github.com/kimkeonlee">
+                김건이
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                {' '}
-                Licenses{' '}
+              <a className="nav-link" href="https://github.com/LeeSeungJi27">
+                이승지
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="https://github.com/17season">
+                박선우
               </a>
             </li>
             <li>
-              <Chatgpt/>
+              <Chatgpt />
             </li>
           </ul>
         </nav>
         <div className="copyright">
-          QR코드 기반 케이블 관리자 시스템<i className="fa fa-heart heart text-danger"></i> by
-          <a href="http://www.themekita.com">ThemeKita</a>
+          QR코드 기반 케이블 관리자 프로그램
         </div>
         <div>
-          Distributed by
+          큐랑께
           <a
             target="_blank"
-            href="https://themewagon.com/"
+            href="https://github.com/2024-SMHRD-SW-Fullstack-1/Qrancae"
             rel="noopener noreferrer"
           >
-            ThemeWagon
+            GIT HUB
           </a>
           .
         </div>
