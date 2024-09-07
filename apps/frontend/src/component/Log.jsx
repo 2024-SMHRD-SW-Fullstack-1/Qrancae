cd9c7d7e2ddcd26e718f2b776459b539843dfdd0
hyoj1201 <kyshs45@naver.com>
2024-09-07 19:40:31 +0900

    풀 받기

--- apps/frontend/src/component/Log.jsx
+++ apps/frontend/src/component/Log.jsx
@@ -26,8 +26,10 @@ const Log = () => {
   const [dateRange, setDateRange] = useState([null, null]);
   const [users, setUsers] = useState([]);
   const [selectedUser, setSelectedUser] = useState('All');
+  const [loading, setLoading] = useState(false); // 로딩중인지 확인
 
   useEffect(() => {
+    setLoading(true);
     // 오늘 날짜로 초기 날짜 범위 설정
     const today = new Date();
     const startOfDay = new Date(today.setHours(0, 0, 0, 0));  // 오늘 0시
@@ -61,6 +63,7 @@ const Log = () => {
       const response = await axios.get('http://localhost:8089/qrancae/api/getlog');
       setLogdata(response.data);
       setFilteredData(response.data);
+      setLoading(false);
     } catch (error) {
       console.error('로그 데이터 오류:', error);
     }
@@ -178,6 +181,15 @@ const Log = () => {
 
       <div className="main-panel">
         <Header />
+        {loading && (
+          <div className="overlay">
+            <img
+              src="assets/img/spinner.svg"
+              alt="Loading..."
+              className="spinner"
+            />
+          </div>
+        )}
 
         <div className="container">
           <div className="page-inner">
