a2f76e0f88e07d695771f1e14f5ada2537a1c07e
이승지 <162680066+LeeSeungJi27@users.noreply.github.com>
2024-09-09 12:31:06 +0900

    요청 작업자 팝업 수정

--- node_modules/test/src/component/Repair.jsx
+++ node_modules/test/src/component/Repair.jsx
@@ -424,7 +424,7 @@ const Repair = () => {
                     <ModalPopup
                         isOpen={showNonePopup}
                         onClose={closeNonePopup}
-                        message="선택된 작업자가 없습니다."
+                        message="선택된 내역이 없습니다."
                     />
                 )}
                 {showCompletePopup && (
@@ -578,13 +578,13 @@ const Repair = () => {
                                             // 상태 메시지를 배열로 수집
                                             const errorMessages = [];
                                             if (item.maint_qr === '불량') {
-                                                errorMessages.push(<span key="qr">QR 상태( <span style={{ color: 'red' }}>{item.maint_qr}</span> )</span>);
+                                                errorMessages.push(<span key="qr">QR 상태(<span style={{ color: 'red' }}>{item.maint_qr}</span>)</span>);
                                             }
                                             if (item.maint_cable === '불량') {
-                                                errorMessages.push(<span key="cable">케이블 상태( <span style={{ color: 'red' }}>{item.maint_cable}</span> )</span>);
+                                                errorMessages.push(<span key="cable">케이블 상태(<span style={{ color: 'red' }}>{item.maint_cable}</span>)</span>);
                                             }
                                             if (item.maint_power === '불량') {
-                                                errorMessages.push(<span key="power">전원 공급 상태( <span style={{ color: 'red' }}>{item.maint_power}</span> )</span>);
+                                                errorMessages.push(<span key="power">전원 공급 상태(<span style={{ color: 'red' }}>{item.maint_power}</span>)</span>);
                                             }
 
                                             // 상태 메시지들을 쉼표로 구분하여 문자열로 변환
